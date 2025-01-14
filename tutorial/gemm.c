5fa12b3bac85cd67e97d8b5920356bbc51968094
micropuma <micropuma@163.com>
2025-01-14 17:35:53 +0800

    add source test code

--- tutorial/gemm.c
+++ tutorial/gemm.c
@@ -1,15 +1,12 @@
 #define N 200
 #define M 300
-#define K 400
 #define DATA_TYPE float
 
-void matmul(DATA_TYPE A[N][K], DATA_TYPE B[K][M], DATA_TYPE C[N][M]) {
-  int i, j, k;
+void matadd(DATA_TYPE A[N][M], DATA_TYPE B[N][M], DATA_TYPE C[N][M]) {
   for (int i = 0; i < N; i++) {
     for (int j = 0; j < M; j++) {
-      for (int k = 0; k < K; k++) {
-        C[i][j] += A[i][k] * B[k][j];
-      }
+      C[i][j] = A[i][j] + B[i][j];
     }
   }
 }
+
